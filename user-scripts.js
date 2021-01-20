7bb9d6a61b1ae272cbabc699a9ccbc2e762c4eaa
matt <mattjohnson767@gmail.com>
2021-01-20 18:57:39 -0500

    journal-client

---
+++ user-scripts.js
@@ -0,0 +1,82 @@
+/* *************************
+*** USER SIGNUP ***
+************************** */
+function userSignUp() {
+    //  console.log('userSignUp Function Called')
+
+    let userEmail = document.getElementById('emailSignup').value;
+    let userPass = document.getElementById('pwdSignup').value;
+    let newUserData = { user: { email: userEmail, password: userPass } };
+    console.log(`NEWUSERDATA ==> ${newUserData.user.email}  ${newUserData.user.password}`);
+
+
+fetch('http://localhost:3000/user/create', {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(newUserData)
+  })
+    .then(response => response.json())
+    .then(function (response) {
+        console.log(response.sessionToken);
+        let token = response.sessionToken;
+        localStorage.setItem('SessionToken', token);
+        tokenChecker()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+/* *************************
+*** USER LOGIN ***
+************************** */
+function userLogin() {
+    //  console.log('userSignUp Function Called')
+
+    let userEmail = document.getElementById('emailLogin').value;
+    let userPass = document.getElementById('pwdLogin').value;
+    let UserData = { user: { email: userEmail, password: userPass } };
+    console.log(`NEWUSERDATA ==> ${UserData.user.email}  ${UserData.user.password}`)
+    
+    
+    
+    
+fetch('http://localhost:3000/user/login', {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(UserData)
+})
+.then(response => response.json())
+    .then(function (response) {
+        console.log(response.sessionToken);
+        let token = response.sessionToken;
+        localStorage.setItem('SessionToken', token);
+        tokenChecker()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+}
+    
+/* *************************
+*** USER LOGOUT ***
+************************** */
+function userLogout() {
+    localStorage.setItem('SessionToken',undefined)
+    console.log(`sessionToken ==> ${localStorage.sessionToken}`)
+    tokenChecker()
+}
+
+
+/* *************************
+ *** TOKEN CHECKER FUNCTION ***
+************************** */
+function tokenChecker() {
+    console.log('tokenChecker Function Called')
+}
+tokenChecker()
